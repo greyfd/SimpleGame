c41c510b980d00f15df5b378d92219a8ead76504
greyfd <greysonfox6464@gmail.com>
2026-01-15 08:09:51 -0600

    better jump button

--- gameNotesDobbelsIOS/GameScene.swift
+++ gameNotesDobbelsIOS/GameScene.swift
@@ -24,6 +24,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var removedItems: [SKNode] = []
     
+    var canJump = true;
+    
     var vc: GameViewController!
     //ONCE
     override func didMove(to view: SKView) {
@@ -61,7 +63,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             score += 1
             scoreLabel.text = "Score: \(score)"
             removedItems.append(coin)
-            coin.removeFromParent()
+            removedItems.last!.removeFromParent()
+        }
+        
+        
+        checkContact(nodeName: "ground") { [self] ground in
+            print("touch gorund")
+            canJump = true;
+            vc.jumpButton.isEnabled = true
         }
         
         checkContact(nodeName: "spike") { [self] spike in
@@ -79,16 +88,50 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    func didEnd(_ contact: SKPhysicsContact) {
+        
+        checkContact(nodeName: "ground") { [self] ground in
+            print("end touch gorund")
+            canJump = false;
+            vc.jumpButton.isEnabled = false
+        }
+        
+        
+        
+        
+        
+        func checkContact(nodeName: String, action: ((SKNode) -> Void)?) {
+            if((contact.bodyA.node?.name == nodeName || contact.bodyB.node?.name == nodeName) && (contact.bodyA.node?.name == "player" || contact.bodyB.node?.name == "player")) {
+                
+                let node = contact.bodyA.node?.name == nodeName ? contact.bodyA.node : contact.bodyB.node
+
+                action?(node!)
+            }
+        }
+    }
+    
     func die() {
+        
         let action = SKAction.run {
             self.player.position = CGPoint(x: 0, y: 0)
         }
+            
+        player.run(action)
         
         for node in removedItems {
             self.addChild(node)
         }
-            
-        player.run(action)
+    }
+    
+    func jump() {
+        if(canJump) {
+            print("jump")
+            let action = SKAction.run {
+                self.player.physicsBody?.velocity.dy = 750
+            }
+            player.run(action)
+
+        }
     }
     
     
--- gameNotesDobbelsIOS/GameViewController.swift
+++ gameNotesDobbelsIOS/GameViewController.swift
@@ -12,7 +12,10 @@ import GameplayKit
 class GameViewController: UIViewController {
     
     let screenSize: CGRect = UIScreen.main.bounds
-
+    var jumpButton: UIButton!
+    
+    @IBOutlet weak var jumpOutlet: UIButton!
+    
     
     var button: UIButton = {
         let button = UIButton(type: .system)
@@ -20,6 +23,8 @@ class GameViewController: UIViewController {
         button.setImage(UIImage(systemName: "arrow.trianglehead.clockwise"), for: .application)
         return button
     }()
+    
+    
     
     var frame: UIView {
         let frame = UIView(frame: screenSize)
@@ -30,6 +35,35 @@ class GameViewController: UIViewController {
     var scene2: GameScene?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        jumpButton = {
+                let button = UIButton(type: .system)
+                button.setTitle("Jump", for: .normal)
+                button.setTitle("Can't Jump", for: .disabled)
+            let imageConfig = UIImage.SymbolConfiguration(scale: .large)
+                    button.setImage(UIImage(systemName: "arrow.up", withConfiguration: imageConfig), for: .normal)
+                    
+                    button.semanticContentAttribute = .forceRightToLeft
+                    
+                    button.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+                    button.tintColor = .white
+                
+                button.sizeToFit()
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            
+                let xPos = screenSize.width - 300
+                    let yPos = screenSize.height - 120
+                    
+                    button.center = CGPoint(x: xPos, y: yPos)
+                let buttonAction = UIAction(handler: { [self]_ in
+                    scene2?.jump()
+                })
+                button.addAction(buttonAction, for: .touchUpInside)
+                
+                return button
+            }()
+        
         
         let value = UIInterfaceOrientation.landscapeLeft.rawValue
                     UIDevice.current.setValue(value, forKey: "orientation")
@@ -51,6 +85,13 @@ class GameViewController: UIViewController {
             
             view.showsFPS = true
             view.showsNodeCount = true
+            view.addSubview(jumpButton)
+            NSLayoutConstraint.activate([
+                            // Pin to the Right side (minus 40 padding)
+                            jumpButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
+                            // Pin to the Bottom (minus 20 padding)
+                            jumpButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+                        ])
         }
     }
 
@@ -64,7 +105,9 @@ class GameViewController: UIViewController {
     
     
     @IBAction func jumpAction(_ sender: UIButton) {
-        print("jump")
-        scene2?.player.physicsBody?.velocity.dy += 500
+        if(scene2?.canJump == true) {
+            print("jump")
+            scene2?.player.physicsBody?.velocity.dy += 500
+        }
     }
 }
